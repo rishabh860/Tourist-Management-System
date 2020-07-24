be38c5d55cc222503f6829d117eab161d91b46cd
rishabh860 <64364798+rishabh860@users.noreply.github.com>
2020-07-24 10:01:51 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -11,9 +11,7 @@
 			<ul>
 				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
 				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
-				
 				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
-				
 			</ul>
 		</div>
 		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2019 TMS . All Rights Reserved </p>
